8ad6684ddd694d49c27458ee21e602c3c8851a0c
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2024-08-20 11:38:49 +0200

    Update code

--- src/go/hello.go
+++ src/go/hello.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("Fibonnaci(12): ", fibonnaci(12))
+}
+
+func fibonnaci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	var n2, n1 = 0, 1
+	for i := 2; i <= n; i++ {
+		n2, n1 = n1, n1+n2
+	}
+	return n1
 }
