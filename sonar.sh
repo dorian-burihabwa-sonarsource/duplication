21ae15bd2858e89fba0d108694e3a174a79cbd9c
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2024-08-20 12:27:23 +0200

    Some more fine-tuning

--- sonar.sh
+++ sonar.sh
@@ -1,18 +1,20 @@
 #! /usr/bin/env bash
+readonly PROJECT_KEY="duplication"
 
 main() {
+    source ./secrets.env
     if [[ "${#}" -eq 0 ]]; then
         sonar-scanner \
-            -Dsonar.projectKey=no-ruby-duplication-detected \
+            -Dsonar.projectKey="${PROJECT_KEY}" \
             -Dsonar.sources=. \
             -Dsonar.host.url=http://localhost:9000 \
-            -Dsonar.token=sqp_99f5f559de400ee6070fc785715b1315416990c8
+            -Dsonar.token="${SONAR_TOKEN}"
     elif [[ "${#}" -eq 1 && "${1}" == "branch" ]]; then
         sonar-scanner \
-            -Dsonar.projectKey=no-ruby-duplication-detected \
+            -Dsonar.projectKey="${PROJECT_KEY}" \
             -Dsonar.sources=. \
             -Dsonar.host.url=http://localhost:9000 \
-            -Dsonar.token=sqp_99f5f559de400ee6070fc785715b1315416990c8
+            -Dsonar.token="${SONAR_TOKEN}"
     fi
 }
 
