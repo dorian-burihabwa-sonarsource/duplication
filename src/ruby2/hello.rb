bc0f5d759123abe14bfd6cac1df73dd4e2e515d6
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2024-08-20 11:49:04 +0200

    Some more copying

--- src/ruby2/hello.rb
+++ src/ruby2/hello.rb
@@ -7,12 +7,34 @@ def fib(n)
     end
     puts first_num
 end
+
+def fib2(n)
+    first_num, second_num = [0, 1]
+    (n - 1).times do
+        first_num, second_num = second_num, first_num + second_num
+    end
+    puts first_num
+end
   
 
 def main()
     puts("Hello, World!")
     print("Fibonnaci(12): ")
     puts(fib(12))
+    print("Fibonnaci(12): ")
+    puts(fib2(12))
+    print("Fibonnaci(12): ")
+    puts(fib(12))
+    print("Fibonnaci(12): ")
+    puts(fib2(12))
+    print("Fibonnaci(12): ")
+    puts(fib(12))
+    print("Fibonnaci(12): ")
+    puts(fib2(12))
+    print("Fibonnaci(12): ")
+    puts(fib(12))
+    print("Fibonnaci(12): ")
+    puts(fib2(12))
 end
 
 main()
