75aae3fbb626fa6f6b7d7f72fa9fe0923470e22d
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2024-08-20 11:40:31 +0200

    Update duplication

--- src/ruby2/hello.rb
+++ src/ruby2/hello.rb
@@ -1,7 +1,18 @@
 #! /usr/bin/env ruby
 
+def fib(n)
+    first_num, second_num = [0, 1]
+    (n - 1).times do
+        first_num, second_num = second_num, first_num + second_num
+    end
+    puts first_num
+end
+  
+
 def main()
     puts("Hello, World!")
+    print("Fibonnaci(12): ")
+    puts(fib(12))
 end
 
 main()
